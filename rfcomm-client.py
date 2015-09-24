708565fbc28411956e1dd1e4319d484a91929e9e
German Om <germanom91@gmail.com>
2015-09-24 12:31:11 -0700

    trying old commented code

--- rfcomm-client.py
+++ rfcomm-client.py
@@ -1,35 +1,36 @@
-#
-# import bluetooth
-#
-# bd_addr = "C0:33:5E:22:C3:68"
-# port = 1
-#
-# sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-# sock.connect((bd_addr, port))
-#
-# sock.send("hello!!")
-#
-# sock.close()
 
-
-import sys
 import bluetooth
 
-uuid = "08C2B2EF-7C87-3D00-0CDC-9A2ADC420BFF"
-service_matches = bluetooth.find_service( uuid = uuid )
-
-if len(service_matches) == 0:
-    print "couldn't find the FooBar service"
-    sys.exit(0)
-
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
-
-print "connecting to \"%s\" on %s" % (name, host)
+bd_addr = "C0:33:5E:22:C3:68"
+port = 1
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-sock.connect((host, port))
+sock.connect((bd_addr, port))
+
 sock.send("hello!!")
+
 sock.close()
+
+#
+# import sys
+# import bluetooth
+#
+# target_address = "C0:33:5E:22:C3:68"
+# service_matches = bluetooth.find_service(address=target_address)
+#
+# if len(service_matches) == 0:
+#     print "couldn't find the FooBar service"
+#     sys.exit(0)
+#
+# first_match = service_matches[0]
+# port = first_match["port"]
+# name = first_match["name"]
+# host = first_match["host"]
+# print port, name, host
+#
+# print "connecting to \"%s\" on %s" % (name, host)
+#
+# sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+# sock.connect((host, 1))
+# sock.send("hello!!")
+# sock.close()
