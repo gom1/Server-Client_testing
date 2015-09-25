a796352f4d8fdfa32eee38762bb3a7c55dceb81f
German Om <germanom91@gmail.com>
2015-09-25 11:39:00 -0700

    changed port# to 5 (dedicated port for ROIE)

--- rfcomm-client.py
+++ rfcomm-client.py
@@ -2,14 +2,15 @@
 import bluetooth
 
 bd_addr = "C0:33:5E:22:C3:68"
-port = 0
+port = 5
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 
-sock.send("hello!!")
+sock.send("hello!! 21321")
 
 sock.close()
+
 #
 # import sys
 # import bluetooth
