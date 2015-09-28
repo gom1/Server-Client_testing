447c375492e8a04809e7d3d48c7d4e751418ad2e
German Om <germanom91@gmail.com>
2015-09-28 09:03:01 -0700

    deleted commented code

--- rfcomm-client.py
+++ rfcomm-client.py
@@ -10,27 +10,3 @@ sock.connect((bd_addr, port))
 sock.send("hello!! 21321")
 
 sock.close()
-
-#
-# import sys
-# import bluetooth
-#
-# target_address = "C0:33:5E:22:C3:68"
-# service_matches = bluetooth.find_service(address=target_address)
-#
-# if len(service_matches) == 0:
-#     print "couldn't find the FooBar service"
-#     sys.exit(0)
-#
-# first_match = service_matches[0]
-# port = first_match["port"]
-# name = first_match["name"]
-# host = first_match["host"]
-# print port, name, host
-#
-# print "connecting to \"%s\" on %s" % (name, host)
-#
-# sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-# sock.connect((host, 1))
-# sock.send("hello!!")
-# sock.close()
--- rfcomm-server.py
+++ rfcomm-server.py
@@ -14,24 +14,3 @@ print "received [%s]" % data
 
 client_sock.close()
 server_sock.close()
-
-# import bluetooth
-#
-# server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-#
-# port = bluetooth.get_available_port( bluetooth.RFCOMM )
-# server_sock.bind(("", port))
-# server_sock.listen(1)
-# print "listening on port %d" % port
-#
-# uuid = "08C2B2EF-7C87-3D00-0CDC-9A2ADC420BFF"
-# bluetooth.advertise_service(server_sock, "FooBar Service", uuid)
-#
-# client_sock, address = server_sock.accept()
-# print "Accepted connection from ", address
-#
-# data = client_sock.recv(1024)
-# print "received [%s]" % data
-#
-# client_sock.close()
-# server_sock.close()
