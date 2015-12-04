1c885c138cf7d7861946a2ed24c0df8fa6c55714
German Om <germanom91@gmail.com>
2015-12-03 17:44:59 -0800

    add loop to poll info indefinitely

--- rfcomm-client.py
+++ rfcomm-client.py
@@ -1,13 +1,24 @@
 
 import bluetooth
 
-bd_addr = "C0:33:5E:22:C3:68"
+bd_addr = "2C:54:CF:79:CC:24"
 port = 5
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 
-sock.send("hello!! 21321")
+done = False
+while done != True:
+    temp = raw_input("Message: ")
+    sock.send(temp)
+    incoming = sock.recv(1024)
+    print incoming
+
+    if temp == "1":
+        done=False
+        print "hi"
+
+
 
 sock.close()
 
